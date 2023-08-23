7412e4683d7ac72c55f4c93fb57197ea566ffc35
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2023-08-23 15:52:33 -0300

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewUserComponent } from './new-user/new-user.component';
+import { AllUsersComponent } from './all-users/all-users.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'create', component: NewUserComponent },
+  { path: 'user/:id', component: UserComponent },
+  { path: 'users', component: AllUsersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
